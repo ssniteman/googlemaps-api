f943dd8c7ce373dbddcb25ac42536f8561cd82ac
Shane Sniteman <shanesniteman@Shanes-MacBook-Pro.local>
2014-03-05 16:27:47 -0500

    Map on greenville, sc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(34.8272504, -82.3614572),
+      zoom: 12
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
